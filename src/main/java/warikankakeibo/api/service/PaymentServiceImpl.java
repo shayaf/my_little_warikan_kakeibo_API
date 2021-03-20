317c58590b80ac6a60f07cf09ff4c1d342470b1e
shayaf <saya2yn@gmail.com>
2021-03-20 23:44:19 +0900

    bug Fix

--- src/main/java/warikankakeibo/api/service/PaymentServiceImpl.java
+++ src/main/java/warikankakeibo/api/service/PaymentServiceImpl.java
@@ -1,10 +1,12 @@
 package warikankakeibo.api.service;
 
+import org.springframework.stereotype.Service;
 import warikankakeibo.api.dao.PaymentDao;
 import warikankakeibo.api.domain.Payment;
 
 import java.util.List;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
 
     @Override
