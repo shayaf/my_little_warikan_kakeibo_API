88d17488b5bd8a2c64916c04e61cdb4513485555
shayaf <saya2yn@gmail.com>
2021-06-05 00:24:04 +0900

    プロジェクトの支払い全取得を追加

--- src/main/java/warikankakeibo/api/controller/appController.java
+++ src/main/java/warikankakeibo/api/controller/appController.java
@@ -1,14 +1,29 @@
 package warikankakeibo.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import warikankakeibo.api.domain.Payment;
+import warikankakeibo.api.service.PaymentService;
+
+import java.util.List;
 
 @RestController
 public class appController {
+
+    private final PaymentService paymentService;
+
+    public appController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     @GetMapping("/")
     public String index() {
         return "app top";
     }
 
+    @GetMapping("/payment")
+    public List<Payment> getPaymentAll(@RequestParam("project_id") Integer projectId) {
+        return paymentService.getAll(projectId);
+    }
 }
